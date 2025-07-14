d36add1b6abcda2a9976f6b9dda0df3b4b542236
oregonyuky <felipe.akio.nishimura@gmail.com>
2025-07-14 12:54:13 -0300

    mapa feito

---
+++ Exercicios resolvidos/Maps/Maps/MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsApp.swift
+//  Maps
+//
+//  Created by Turma01-5 on 08/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
